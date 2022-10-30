a5ca1c5ee5841bbd1db86874398a39b3a7a41c45
Sanketh B K <51091231+SankethBK@users.noreply.github.com>
2022-10-30 19:52:25 +0530

    fixed dummy test

--- test/flutter_sticky_buttons_test.dart
+++ test/flutter_sticky_buttons_test.dart
@@ -1,12 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_sticky_buttons/flutter_sticky_buttons.dart';
+import 'package:flutter_sticky_widgets/flutter_sticky_buttons.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  test('adds one to input values', () {});
 }
